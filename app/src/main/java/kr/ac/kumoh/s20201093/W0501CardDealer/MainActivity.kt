5862ee146e7be94d94dbc400544d8a219de9547f
hyeji <hyejizip@gmail.com>
2022-10-11 14:49:51 +0900

    프로젝트 수정 02

--- app/src/main/java/kr/ac/kumoh/s20201093/W0501CardDealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201093/W0501CardDealer/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.s20201093.W0501CardDealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import kr.ac.kumoh.s20201093.W0501CardDealer.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -13,14 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
-        setContentView(binding.root) //이 상태에서는 하트3
+        setContentView(binding.root)
+
+        binding.btnDeal.setOnClickListener{
+            val res = IntArray(5)
+            for(i in 0 until res.size) {
+                res[i] = resources.getIdentifier(
+                    getCardName(Random.nextInt(52)), //string으로 찾음
+                    "drawable",
+                    packageName
+                )
+            }
+            binding.card1.setImageResource(res[0]) //컴파일타입에 의해 결정
+            binding.card2.setImageResource(res[1]) //세로에다 234 만들어놓고 가로에는 1밖에 없음
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
 
         //binding.card1.setImageResource(R.drawable.c_10_of_spades)
         //10스페이드로 변경됨
-
-        getCardName(29)
-
-        binding.card1.setImageResource(R.drawable.c_10_of_spades)
     }
     private fun getCardName(c: Int) : String {
         //int 하나 받고, 리턴값 String
@@ -35,11 +48,15 @@ class MainActivity : AppCompatActivity() {
         Log.i("getCardName() : ", shape)
 
         val number = when (c % 13) {
+            0 -> "ace" //나머지가 0일 때
             in 1..9 ->(c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
             else -> "error"
         }
         Log.i("getCardName() : ", number)
 
-        return "c_ace_od_spades"
+        return "c_${number}_of_${shape}" //실시간으로 사용하는 거
     }
 }
